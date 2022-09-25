d5316e85e6f6388d5d9835c3d397365c61ee2902
Maria Osorio <mosorio@nsven.com>
2022-09-25 18:58:11 -0400

    some progress

--- client/src/Components/Static/English/NavBar/NavBar.tsx
+++ client/src/Components/Static/English/NavBar/NavBar.tsx
@@ -1,6 +1,15 @@
 import { Link } from 'react-router-dom'
 import '../../../../Styles/NavBar/NavBar.scss'
 export default function NavBar() {
+    const updateBar = () => {
+        const bodyEl:any = document.body
+        const barEl:any = document.querySelector('.bar')
+        let scrollPos = (window.scrollY / (bodyEl.scrollHeight - window.innerHeight) * 100)
+        barEl.style.width = `${scrollPos}%`
+        requestAnimationFrame(updateBar)
+    } 
+    updateBar()
+
     return (
         <section className='NavBar-Container'>
             <div className='Logo-Container'>
@@ -9,18 +18,21 @@ export default function NavBar() {
                 </Link>
             </div>
             <div className='NavBar-Content-Container'>
-                <Link to='/AboutMe'>
+                <Link to='/AboutMe' className='link'>
                     <span>About Me</span>
                 </Link>
-                <Link to='/Projects'>
+                <Link to='/Projects' className='link'>
                     <span>Projects</span>
                 </Link>
-                <Link to='/MySkills'>
+                <Link to='/MySkills' className='link'>
                     <span>My Skills</span>
                 </Link>
-                <Link to='/Contact'>
+                <Link to='/Contact' className='link'>
                     <span>Contact</span>
                 </Link>
+                </div>
+                <div className='progress'>
+                    <span className='bar'></span>
                 </div>
         </section>
     )
