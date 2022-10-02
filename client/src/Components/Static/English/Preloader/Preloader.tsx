197b9e3e6a4f28bd92f4e6874b9750204fec2a79
Maria Osorio <mosorio@nsven.com>
2022-10-02 04:59:53 -0400

    almosssst

---
+++ client/src/Components/Static/English/Preloader/Preloader.tsx
@@ -0,0 +1,11 @@
+import '../../../../Styles/Preloader/Preloader.scss'
+
+const Preloader = () => {
+    return (
+        <div className='Preloader-container'>
+            <img src="/favicon-1.svg" alt="Andrea-saying-Hi" />
+        </div>
+    )
+}
+
+export default Preloader
