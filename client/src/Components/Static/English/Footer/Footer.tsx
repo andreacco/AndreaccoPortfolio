df41fdcb58821aee1e6500acc03c5a6901306096
Maria Osorio <mosorio@nsven.com>
2022-10-03 02:57:43 -0400

    fixed some responsive styles

--- client/src/Components/Static/English/Footer/Footer.tsx
+++ client/src/Components/Static/English/Footer/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <div className='Footer-container'>
       <h5>Designed and developed by Andrea Carvajal Osorio.</h5>
-      <h5>The ilustrations were provided by <a href='https://zepeto.me/'>Zepeto @andreeacco</a> and <a href='https://www.flaticon.com/'>flaticon.</a></h5>
+      <h5 className='second-line'>The ilustrations were provided by <a href='https://zepeto.me/'>Zepeto @andreeacco</a> and <a href='https://www.flaticon.com/'>flaticon.</a></h5>
     </div>
   )
 }
